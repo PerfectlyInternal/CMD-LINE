87e96aa7569f2ad6fc061dab23213896e39e35f1
SashaM69420 <alex.n.martynov@gmail.com>
2020-04-17 16:18:43 -0400

    Added server class and fixed connectivity issues

--- CMD_LINE.py
+++ CMD_LINE.py
@@ -10,7 +10,7 @@ class socket_wrapper :
         if socket == None :
             self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
         else :
-            self.sock = sock
+            self.socket = socket
 
     def connect(self, host, port) :
         self.socket.connect((host, port))
@@ -18,7 +18,7 @@ class socket_wrapper :
     def send(self, msg) :
         total_sent = 0
         while total_sent < len(msg) :
-            sent = self.sock.send(msg[total_sent:])
+            sent = self.socket.send(msg[total_sent:])
             if sent == 0 :
                 raise RuntimeError("Socket connection was broken. Please restart and try again")
             total_sent += sent
@@ -27,24 +27,41 @@ class socket_wrapper :
         chunks = []
         bytes_recd = 0
         while bytes_recd < MSGLEN:
-            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            chunk = self.socket.recv(min(MSGLEN - bytes_recd, 2048))
             if chunk == b'':
-                raise RuntimeError("socket connection broken")
+                raise RuntimeError("Socket connection was broken. Please restart and try again")
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
         return b''.join(chunks)
 
 class server :
-    
+    # A simple class to handle server creation
+    # May need to be put on a separate thread later
+
+    def __init__(self, socket = None) :
+        if socket == None :
+            self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        else :
+            self.socket = socket
+
+        self.socket.bind(('', 8080))
+
+        self.socket.listen(5)
+
+    def run(self) :
+        while True :
+            client_socket, address = self.socket.accept()
+            print(str(address) + " Has just connected")
 
 def main():
-    print("test")
+    a = input()
 
-    main_socket = socket_wrapper()
-
-    main_socket.connect("localhost", 8080)
-    main_socket.send("aaa")
-    print(main_socket.recieve())
+    if a == "server" :
+        main_server = server(sock.socket(sock.AF_INET, sock.SOCK_STREAM))
+        main_server.run()
+    else :
+        client = socket_wrapper(sock.socket(sock.AF_INET, sock.SOCK_STREAM))
+        client.connect('localhost', 8080)
 
 if __name__ == "__main__" :
     main()
